14ff61caf785136862aebe463fc68655e505993b
Kabilesh J <95236130+Kabi-45@users.noreply.github.com>
2025-03-02 12:13:46 +0530

    Create lengthOfLastWord.py

---
+++ Arrays/lengthOfLastWord.py
@@ -0,0 +1,40 @@
+def lengthOfLastWord(s: str) -> int:
+        last = len(s) - 1
+        while s[last] == " ":
+            last -= 1
+        start = last
+        while s[start] != " " and start >= 0:
+            start -= 1
+        return last - start
+
+print(lengthOfLastWord("Hello World")) #5
+'''
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+
+ 
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of only English letters and spaces ' '.
+There will be at least one word in s.
+'''
